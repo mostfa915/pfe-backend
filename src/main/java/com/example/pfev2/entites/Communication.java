0e9b7fff0b367e6aa861be53bd24356ea3bea471
mostfa915 <mostfa@gmail.com>
2025-05-18 17:16:03 +0100

    ton message

---
+++ src/main/java/com/example/pfev2/entites/Communication.java
@@ -0,0 +1,31 @@
+package com.example.pfev2.entites;
+
+// model/Communication.java
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+@Entity
+@Data
+public class Communication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+
+    public Communication( String sms) {
+   this.content = sms;
+    }
+
+    public Communication() {
+
+    }
+
+    // Méthode pour envoyer une notification
+    public void envoyerNotification() {
+        // Implémentation de l'envoi de notification
+    }
+}
